68215320e6bbea32bf953823b2a36f8a0f2d1532
Rohit Handique <rohand.181cv135@nitk.edu.in>
2020-07-05 11:42:36 +0530

    initial commit
    added main section

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("mySidenav").style.display = "block";
+  document.getElementById("mySidenav").style.transitionDuration = "2s";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.display = "none";
+  document.getElementById("mySidenav").style.transitionDuration = "2s";
+}
