2717152f2d633ad58f5674077617006ac9929469
Rohit Handique <rohand.181cv135@nitk.edu.in>
2020-07-07 16:16:33 +0530

    added events section & one-page-scroll.js

--- src/js/index.js
+++ src/js/index.js
@@ -8,15 +8,3 @@ function closeNav() {
   document.getElementById("sidenav").style.display = "none";
   document.getElementById("sidenav").style.transitionDuration = "2s";
 }
-
-//sticky header on scroll
-window.onscroll = function () {
-  myFunction();
-};
-var header = document.getElementById("header");
-var sticky = header.offsetTop;
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    header.classList.add("sticky");
-  }
-}
