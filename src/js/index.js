dc710d36b13b4c449543c94b9dc8efc91fad7aeb
Rohit Handique <rohand.181cv135@nitk.edu.in>
2020-07-14 13:42:02 +0530

    removed scroll js library

--- src/js/index.js
+++ src/js/index.js
@@ -8,13 +8,3 @@ function closeNav() {
   document.getElementById("sidenav").style.display = "none";
   document.getElementById("sidenav").style.transitionDuration = "2s";
 }
-
-//fullpage scroll
-new fullpage("#fullpage", {
-  //options here
-  autoScrolling: true,
-  scrollHorizontally: true,
-});
-
-//methods
-fullpage_api.setAllowScrolling(false);
