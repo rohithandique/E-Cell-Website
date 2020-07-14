bb357beae8b7ac6c4a0dbe5ad94a6fd88dba7019
Rohit Handique <rohand.181cv135@nitk.edu.in>
2020-07-14 13:34:45 +0530

    fixed few things, added blog article

--- src/js/index.js
+++ src/js/index.js
@@ -8,3 +8,13 @@ function closeNav() {
   document.getElementById("sidenav").style.display = "none";
   document.getElementById("sidenav").style.transitionDuration = "2s";
 }
+
+//fullpage scroll
+new fullpage("#fullpage", {
+  //options here
+  autoScrolling: true,
+  scrollHorizontally: true,
+});
+
+//methods
+fullpage_api.setAllowScrolling(false);
